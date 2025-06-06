52a61a46071c6412194e717cfcd388fb51c7f613
oladejo-michael <mickeyboy16@gmail.com>
2025-06-06 15:32:13 +0100

    Added some bunch of codes

--- app.py
+++ app.py
@@ -6,5 +6,19 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 
+@app.route('/about')
+def about():
+    return "This is a simple Flask app."
+
+@app.route('/api/data')
+def data():
+    sample_data = {
+        "name": "John Doe",
+        "age": 30,
+        "language": "Spanish",
+        "framwork": "Flask"
+    }
+    return jsonify(sample_data)
+
 if __name__ == '__main__':
     app.run(debug=True)
